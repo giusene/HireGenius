7f51039c917dac49ce2c45cd093a8c034819cda4
Giovanni Raniolo <gianniraniolo80@gmail.com>
2024-09-12 15:23:09 +0200

    feat: implement password recovery logic using Firebase Authentication

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,4 +1,4 @@
-// src/pages/login.tsx
+// src/pages/login/index.tsx
 
 import { useState } from "react";
 import { useAuth } from "@/context/AuthContext";
@@ -14,45 +14,49 @@ import style from "./login.module.scss";
 import Hero from "../../../public/hero.png";
 
 const Login = () => {
-	const [error, setError] = useState("");
-	const { loginWithGoogle, user } = useAuth();
+  const [error, setError] = useState("");
+  const { loginWithGoogle, user } = useAuth();
 
-	const handleGoogleLogin = async () => {
-		try {
-			await loginWithGoogle();
-			alert("Login con Google effettuato con successo!");
-		} catch (err) {
-			setError("Errore durante il login con Google. Riprova.");
-		}
-	};
+  const handleGoogleLogin = async () => {
+    try {
+      await loginWithGoogle();
+      alert("Login con Google effettuato con successo!");
+    } catch (err) {
+      setError("Errore durante il login con Google. Riprova.");
+    }
+  };
 
-	return (
-		<main className={style.main}>
-			<div className={style.hero}>
-				<Image src={Hero} alt='Hero image' width={545} height={204} />
-			</div>
+  return (
+    <main className={style.main}>
+      <div className={style.hero}>
+        <Image src={Hero} alt="Hero image" width={545} height={204} />
+      </div>
 
-			<div className={style.header}>
-				<h1>interViewer</h1>
-				<h2>Testa le tue competenze</h2>
-			</div>
+      <div className={style.header}>
+        <h1>interViewer</h1>
+        <h2>Testa le tue competenze</h2>
+      </div>
 
-			<LoginForm />
+      <LoginForm />
 
-			<hr />
+      <hr />
 
-			<PrimaryButton label='Accedi con Google' className='ctaB' onClick={handleGoogleLogin} />
+      <PrimaryButton
+        label="Accedi con Google"
+        className="ctaB"
+        onClick={handleGoogleLogin}
+      />
 
-			<p className={style.register}>
-				Non hai un account?{" "}
-				<Link className={style.registerLink} href='/register'>
-					Registrati
-				</Link>
-			</p>
+      <p className={style.register}>
+        Non hai un account?{" "}
+        <Link className={style.registerLink} href="/register">
+          Registrati
+        </Link>
+      </p>
 
-			{/* {user && <Link href='/protected'>test</Link>} */}
-		</main>
-	);
+      {/* {user && <Link href='/protected'>test</Link>} */}
+    </main>
+  );
 };
 
 export default Login;
