54254debc7739b990dbf331784a1637ec5c67f0b
majinbru <majinbrum@gmail.com>
2024-09-17 18:29:09 +0200

    navbar icons

--- src/components/Molecules/NavBar/NavBar.tsx
+++ src/components/Molecules/NavBar/NavBar.tsx
@@ -3,53 +3,53 @@ import Link from "next/link";
 import style from "./NavBar.module.scss";
 import { navMenu, footerMenu } from "@/constants/menuData";
 import ActionButton from "@/components/Atoms/Buttons/ActionButton";
-import MenuIcon from "../../../../public/icons/menu-icon.png";
+import MenuIcon from "@/../public/icons/menu-icon.png";
+import Image from "next/image";
 import { useAuth } from "@/context/AuthContext"; // Importa il contesto di autenticazione
 
 const NavBar = () => {
-  const [isOpen, setIsOpen] = useState<boolean>(false);
-  const { user } = useAuth(); // Usa il contesto di autenticazione
+	const [isOpen, setIsOpen] = useState<boolean>(false);
+	const { user } = useAuth(); // Usa il contesto di autenticazione
 
-  const toggleMenu = (): void => {
-    setIsOpen(!isOpen);
-  };
+	const toggleMenu = (): void => {
+		setIsOpen(!isOpen);
+	};
 
-  // Filtra il menu in base all'autenticazione
-  const filteredNavMenu = user
-    ? navMenu
-    : navMenu.filter((item) => item.label === "Home" || item.label === "Login");
+	// Filtra il menu in base all'autenticazione
+	const filteredNavMenu = user ? navMenu : navMenu.filter((item) => item.label === "Home" || item.label === "Login");
 
-  return (
-    <div className={style.navContainer} onMouseLeave={() => setIsOpen(false)}>
-      {!isOpen && (
-        <ActionButton onClick={toggleMenu} className="round" icon={MenuIcon} />
-      )}
+	return (
+		<div className={style.navContainer} onMouseLeave={() => setIsOpen(false)}>
+			{!isOpen && <ActionButton onClick={toggleMenu} className='round' icon={MenuIcon} />}
 
-      {isOpen && (
-        <nav className={style.sidebar}>
-          <ul className={style.menuList}>
-            {filteredNavMenu.map((item) => (
-              <li key={item.label} className={style.menuItem}>
-                <Link href={item.link}>{item.label}</Link>
-              </li>
-            ))}
-          </ul>
+			{isOpen && (
+				<nav className={style.sidebar}>
+					<ul className={style.menuList}>
+						{filteredNavMenu.map((item) => (
+							<li key={item.label}>
+								<Link href={item.link} className={style.menuItem}>
+									<Image src={item.icon} alt={`${item.label} icon`} width={20} height={20} />
+									{item.label}
+								</Link>
+							</li>
+						))}
+					</ul>
 
-          <footer className={style.footer}>
-            <hr />
-            <ul className={style.footerList}>
-              {footerMenu.map((item) => (
-                <li key={item.label} className={style.menuItem}>
-                  <Link href={item.link}>{item.label}</Link>
-                </li>
-              ))}
-              <p>@2024 Interviewer</p>
-            </ul>
-          </footer>
-        </nav>
-      )}
-    </div>
-  );
+					<footer className={style.footer}>
+						<hr />
+						<ul className={style.footerList}>
+							{footerMenu.map((item) => (
+								<li key={item.label}>
+									<Link href={item.link}>{item.label}</Link>
+								</li>
+							))}
+							<p>@2024 Interviewer</p>
+						</ul>
+					</footer>
+				</nav>
+			)}
+		</div>
+	);
 };
 
 export default NavBar;
--- src/constants/menuData.js
+++ src/constants/menuData.js
@@ -1,159 +1,165 @@
+import HomeIcon from "@/../public/icons/home.png";
+import TopicIcon from "@/../public/icons/topic.png";
+import InterviewIcon from "@/../public/icons/interview.png";
+import ProfileIcon from "@/../public/icons/profile.png";
+import SettingsIcon from "@/../public/icons/settings.png";
+import LogoutIcon from "@/../public/icons/logout.png";
+
 export const navMenu = [
-  {
-    label: "Home",
-    link: "/landing-page",
-  },
-  {
-    label: "Nuovo argomento",
-    link: "/new-topic",
-  },
-  {
-    label: "Nuovo colloquio",
-    link: "/new-interview",
-  },
-  {
-    label: "Profilo",
-    link: "/profilo",
-  },
-  {
-    label: "Impostazioni",
-    link: "/impostazioni",
-  },
-  {
-    label: "Logout",
-    link: "/logout",
-  },
-  {
-    label: "Login",
-    link: "/login",
-  },
+	{
+		icon: HomeIcon,
+		label: "Home",
+		link: "/",
+	},
+	{
+		icon: TopicIcon,
+		label: "Nuovo argomento",
+		link: "/topic-process",
+	},
+	{
+		icon: InterviewIcon,
+		label: "Nuovo colloquio",
+		link: "/new-interview",
+	},
+	{
+		icon: ProfileIcon,
+		label: "Profilo",
+		link: "/profilo",
+	},
+	{
+		icon: SettingsIcon,
+		label: "Impostazioni",
+		link: "/impostazioni",
+	},
+	{
+		icon: LogoutIcon,
+		label: "Logout",
+		link: "/logout",
+	},
 ];
 
 export const footerMenu = [
-  {
-    label: "Il progetto",
-    link: "/il-progetto",
-  },
-  {
-    label: "Github",
-    link: "#",
-  },
+	{
+		label: "Il progetto",
+		link: "/il-progetto",
+	},
+	{
+		label: "Github",
+		link: "#",
+	},
 ];
 
 export const loginLabels = {
-  appName: "interViewer",
-  tagline: "Testa le tue competenze",
-  emailLabel: "Email",
-  passwordLabel: "Password",
-  forgotPasswordLink: "Hai dimenticato la password?",
-  buttonLogin: "Accedi",
-  loginWithGoogle: "Accedi con Google",
-  text: "Non hai un account?",
-  action: "Registrati",
+	appName: "interViewer",
+	tagline: "Testa le tue competenze",
+	emailLabel: "Email",
+	passwordLabel: "Password",
+	forgotPasswordLink: "Hai dimenticato la password?",
+	buttonLogin: "Accedi",
+	loginWithGoogle: "Accedi con Google",
+	text: "Non hai un account?",
+	action: "Registrati",
 };
 
 export const registrationFormLabels = {
-  usernameLabel: "Username",
-  roleLabel: "Role",
-  seniorityLabel: "Seniority",
-  email: "Email",
-  password: "Password",
-  confirmPassword: "Confirm password",
-  buttonsRegister: "Registrati",
-  privacyNote:
-    "I tuoi dati saranno protetti secondo GDPR e usati soltanto per personalizzare la tua esperienza.",
+	usernameLabel: "Username",
+	roleLabel: "Role",
+	seniorityLabel: "Seniority",
+	email: "Email",
+	password: "Password",
+	confirmPassword: "Confirm password",
+	buttonsRegister: "Registrati",
+	privacyNote: "I tuoi dati saranno protetti secondo GDPR e usati soltanto per personalizzare la tua esperienza.",
 };
 
 export const recoveryPasswordLabels = {
-  title: "Recupero Password",
-  placeholder: "Inserisci la tua email",
-  buttonRecovery: "Recupera Password",
-  messagesSuccess:
-    "La tua richiesta è stata inviata con successo. Controlla la tua email per ulteriori istruzioni.",
-  messagesError:
-    "Si è verificato un errore durante il recupero della password. Per favore, riprova.",
-  linkText: "Torna al",
-  loginLabel: "Login",
-  href: "/login",
+	title: "Recupero Password",
+	placeholder: "Inserisci la tua email",
+	buttonRecovery: "Recupera Password",
+	messagesSuccess: "La tua richiesta è stata inviata con successo. Controlla la tua email per ulteriori istruzioni.",
+	messagesError: "Si è verificato un errore durante il recupero della password. Per favore, riprova.",
+	linkText: "Torna al",
+	loginLabel: "Login",
+	href: "/login",
 };
 
 export const newInterviewFormLabels = {
-  newInterviewTitle: "Nuovo colloquio",
-  roleLabel: "Ruolo",
-  seniorityLabel: "Seniority",
-  numberOfQuestionsLabel: "Numero di domande",
-  testDurationLabel: "Durata del test",
-  buttonsNext: "Avanti",
+	newInterviewTitle: "Nuovo colloquio",
+	roleLabel: "Ruolo",
+	seniorityLabel: "Seniority",
+	numberOfQuestionsLabel: "Numero di domande",
+	testDurationLabel: "Durata del test",
+	buttonsNext: "Avanti",
 };
 
 export const interviewFormOptions = {
-  seniority: [
-    { label: "Junior", value: "junior" },
-    { label: "Mid-level", value: "mid-level" },
-    { label: "Senior", value: "senior" },
-  ],
-  numberOfQuestions: [
-    { label: "5", value: "5 domande" },
-    { label: "10", value: "10 domande" },
-    { label: "15", value: "15 domande" },
-  ],
-  testDuration: [
-    { label: "10", value: "10 minuti" },
-    { label: "20", value: "20 minuti" },
-    { label: "30", value: "30 minuti" },
-  ],
+	seniority: [
+		{ label: "Junior", value: "junior" },
+		{ label: "Mid-level", value: "mid-level" },
+		{ label: "Senior", value: "senior" },
+	],
+	numberOfQuestions: [
+		{ label: "5", value: "5 domande" },
+		{ label: "10", value: "10 domande" },
+		{ label: "15", value: "15 domande" },
+	],
+	testDuration: [
+		{ label: "10", value: "10 minuti" },
+		{ label: "20", value: "20 minuti" },
+		{ label: "30", value: "30 minuti" },
+	],
 };
 
 export const customFormLabels = {
-  title: "Nuovo Argomento",
-  topicLabel: "Argomento",
-  level: "Livello",
-  numberOfQuestionsLabel: "Numero di domande",
-  testDurationLabel: "Durata del test",
-  button: "Avanti",
+	title: "Nuovo Argomento",
+	topicLabel: "Argomento",
+	level: "Livello",
+	numberOfQuestionsLabel: "Numero di domande",
+	testDurationLabel: "Durata del test",
+	button: "Avanti",
 };
 
 export const customFormOptions = {
-  optionsLevel: [
-    {
-      label: "Base",
-      value: "Base",
-    },
-    {
-      label: "Intermedio",
-      value: "Intermedio",
-    },
-    {
-      label: "Avanzato",
-      value: "Avanzato",
-    },
-  ],
-  optionsQuestion: [
-    {
-      label: "5 domande",
-      value: "5 domande",
-    },
-    {
-      label: "10 domande",
-      value: "10 domande",
-    },
-    {
-      label: "15 domande",
-      value: "15 domande",
-    },
-  ],
-  optionsDuration: [
-    {
-      label: "10 minuti",
-      value: "10 minuti",
-    },
-    {
-      label: "20 minuti",
-      value: "20 minuti",
-    },
-    {
-      label: "30 minuti",
-      value: "30 minuti",
-    },
-  ],
+	optionsLevel: [
+		{
+			label: "Base",
+			value: "Base",
+		},
+		{
+			label: "Intermedio",
+			value: "Intermedio",
+		},
+		{
+			label: "Avanzato",
+			value: "Avanzato",
+		},
+	],
+	optionsQuestion: [
+		{
+			label: "5 domande",
+			value: "5 domande",
+		},
+		{
+			label: "10 domande",
+			value: "10 domande",
+		},
+		{
+			label: "15 domande",
+			value: "15 domande",
+		},
+	],
+	optionsDuration: [
+		{
+			label: "10 minuti",
+			value: "10 minuti",
+		},
+		{
+			label: "20 minuti",
+			value: "20 minuti",
+		},
+		{
+			label: "30 minuti",
+			value: "30 minuti",
+		},
+	],
 };
