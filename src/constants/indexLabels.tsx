422b67fb020ac5397a90faf05bf6049784277738
Your Name <you@example.com>
2024-09-18 12:32:40 +0200

    fix: constants from js to tsx

--- src/constants/indexLabels.tsx
+++ src/constants/indexLabels.tsx
@@ -1,16 +1,15 @@
 export const headLabels = {
   title: "Interviewer - Simulazioni di Test e Colloqui",
-};
+} as const;
 
 export const heroLabels = {
   title: "Interviewer",
   subtitle: "Preparati ai colloqui come un professionista",
   start: "Comincia adesso!",
-};
-
+} as const;
 
 export const featureLabels = {
   quizBadge: "Quizzes tailored to your interests and skill level.",
   simulatedBadge: "Practice real-world interviews and get instant feedback.",
-  aiBadge: "Practice real-world interviews and get instant feedback."
-}
+  aiBadge: "Practice real-world interviews and get instant feedback.",
+} as const;
