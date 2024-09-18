704b4d618eb31dcadb6b4e4fe2bec35e2828abd9
majinbru <majinbrum@gmail.com>
2024-09-18 20:39:25 +0200

    fixed css landing

--- src/pages/landing-page/index.tsx
+++ src/pages/landing-page/index.tsx
@@ -1,23 +1,25 @@
 import withAuth from "@/middleware/withAuth";
 
 //import StatisticsBox from '../../components/Molecules/StatisticsBox/StatisticsBox';
-import TopicBox from "../../components/Molecules/TopicBox/TopicBox";
-import InterviewBox from "../../components/Molecules/InterviewBox/InterviewBox";
+import TestCard from "../../components/Molecules/TestCard/TestCard";
 import styles from "./landing-page.module.scss";
+
+import topicImage from "@/../public/new-topic.png";
+import interviewImage from "@/../public/new-interview.png";
 
 const LandingPage = () => {
 	return (
-		<div className={styles.container}>
+		<main className={styles.main}>
 			{/* <div className={styles.statisticsSection}>
         <StatisticsBox />
       </div> */}
 
-			<div className={styles.header}>
+			<header className={styles.header}>
 				<h2 className={styles.sectionTitle}>Mettiti alla prova!</h2>
-			</div>
-			<TopicBox />
-			<InterviewBox />
-		</div>
+			</header>
+			<TestCard title='Nuovo argomento' description='Destreggiati in un nuovo argomento, che sia un interrogazione o un semplice quiz.' cardImage={topicImage} />
+			<TestCard title='Nuovo colloquio' description='Testa la tua preparazione simulando un colloquio tecnico di lavoro.' cardImage={interviewImage} />
+		</main>
 	);
 };
 
