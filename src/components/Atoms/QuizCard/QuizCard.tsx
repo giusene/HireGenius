6dbfdb667be7b739b4b077d98a43e767aef44a08
majinbru <majinbrum@gmail.com>
2024-09-19 20:03:54 +0200

    profile session details with the  use of the results list component

--- src/components/Atoms/QuizCard/QuizCard.tsx
+++ src/components/Atoms/QuizCard/QuizCard.tsx
@@ -8,105 +8,93 @@ import style from "./QuizCard.module.scss";
 import arrowSelect from "@/../public/icons/arrow-select.png";
 import arrowUp from "@/../public/icons/arrow-up.png";
 import { formatDistanceToNow } from "date-fns";
-import { useState } from "react";
+import { Dispatch, SetStateAction, useState } from "react";
 import ActionButton from "../Buttons/ActionButton";
 import { deleteInterviewSession } from "@/utils/deleteInterviewSession";
 import { useAuth } from "@/context/AuthContext"; // Assumendo che ci sia un contesto di autenticazione
+import { EvaluationResult, InterviewDetails } from "@/interfaces/interfaces";
+import CtaButton from "@/components/Atoms/Buttons/CtaButton";
 
-interface QuizCardProps {
-  date: string;
-  topic: string;
-  score: number;
-  interviewer: string;
-  message: string;
-  sessionId: string; // Aggiungi l'ID della sessione come prop
-  onDelete: (sessionId: string) => void; // Aggiungi la callback come prop
+interface InterviewSession {
+	sessionId: string;
+	sessionDate: string;
+	interviewDetails: InterviewDetails;
+	evaluationResult: EvaluationResult;
 }
 
-const QuizCard: React.FC<QuizCardProps> = ({
-  date,
-  topic,
-  score,
-  interviewer,
-  message,
-  sessionId,
-  onDelete,
-}) => {
-  const [showDetails, setShowDetails] = useState(false);
-  const { user } = useAuth(); // Recupera l'utente autenticato dal contesto
+interface QuizCardProps {
+	interviewSession: InterviewSession;
+	onDelete: (sessionId: string) => void; // Aggiungi la callback come prop
+	setSelectedInterviewSession: Dispatch<SetStateAction<InterviewSession | null>>;
+}
 
-  let iconSrc = null;
+const QuizCard: React.FC<QuizCardProps> = ({ interviewSession, onDelete, setSelectedInterviewSession }) => {
+	const { user } = useAuth(); // Recupera l'utente autenticato dal contesto
+	const [showFeedback, setShowFeedback] = useState(false);
 
-  const iconMap: Record<string, StaticImageData> = {
-    cup: Cup,
-    silver: Silver,
-    bronze: Bronze,
-    lose: Lose,
-  };
+	let iconSrc = null;
 
-  if (score >= 85) {
-    iconSrc = iconMap.cup;
-  } else if (score >= 65) {
-    iconSrc = iconMap.silver;
-  } else if (score >= 50) {
-    iconSrc = iconMap.bronze;
-  } else {
-    iconSrc = iconMap.lose;
-  }
+	const iconMap: Record<string, StaticImageData> = {
+		cup: Cup,
+		silver: Silver,
+		bronze: Bronze,
+		lose: Lose,
+	};
 
-  const newDate = date.replaceAll(" ", ",");
-  const dateFromNow = formatDistanceToNow(new Date(newDate), {
-    addSuffix: true,
-  });
+	if (interviewSession.evaluationResult.globalEvaluation.points >= 85) {
+		iconSrc = iconMap.cup;
+	} else if (interviewSession.evaluationResult.globalEvaluation.points >= 65) {
+		iconSrc = iconMap.silver;
+	} else if (interviewSession.evaluationResult.globalEvaluation.points >= 50) {
+		iconSrc = iconMap.bronze;
+	} else {
+		iconSrc = iconMap.lose;
+	}
 
-  // Cancellazione
-  const handleDelete = async () => {
-    if (user) {
-      try {
-        await deleteInterviewSession(user.uid, sessionId);
-        onDelete(sessionId); // Chiama la funzione di eliminazione del parent
-      } catch (error) {
-        console.error("Errore durante la cancellazione della sessione:", error);
-      }
-    }
-  };
+	const newDate = interviewSession.sessionDate.replaceAll(" ", ",");
+	const dateFromNow = formatDistanceToNow(new Date(newDate), {
+		addSuffix: true,
+	});
 
-  return (
-    <div className={style.quizCardContainer}>
-      <div className={style.quizCard}>
-        <Image
-          src={iconSrc}
-          className={style.icon}
-          alt="Icon"
-          width={80}
-          height={80}
-        />
-        <div className={style.content}>
-          <span>{dateFromNow}</span>
-          <h4>{topic}</h4>
-          <h4>
-            Punteggio: <span>{score}/100</span>
-          </h4>
-          <h4>
-            Intervistatore: <span>{interviewer}</span>
-          </h4>
-        </div>
-        <div className={style.buttons}>
-          <ActionButton
-            className="round"
-            icon={DeleteIcon}
-            onClick={handleDelete}
-          />
-          <ActionButton
-            className="round"
-            icon={showDetails ? arrowUp : arrowSelect}
-            onClick={() => setShowDetails(!showDetails)}
-          />
-        </div>
-      </div>
-      {showDetails && <p>{message}</p>}
-    </div>
-  );
+	// Cancellazione
+	const handleDelete = async () => {
+		if (user) {
+			try {
+				await deleteInterviewSession(user.uid, interviewSession.sessionId);
+				onDelete(interviewSession.sessionId); // Chiama la funzione di eliminazione del parent
+			} catch (error) {
+				console.error("Errore durante la cancellazione della sessione:", error);
+			}
+		}
+	};
+
+	return (
+		<div className={style.quizCardContainer}>
+			<div className={style.quizCard}>
+				<Image src={iconSrc} className={style.icon} alt='Icon' width={80} height={80} />
+				<div className={style.content}>
+					<span>{dateFromNow}</span>
+					<h4>{interviewSession.interviewDetails.topic}</h4>
+					<h4>
+						Punteggio: <span>{interviewSession.evaluationResult.globalEvaluation.points}/100</span>
+					</h4>
+					<h4>
+						Intervistatore: <span>{interviewSession.interviewDetails.interviewer.name}</span>
+					</h4>
+				</div>
+				<div className={style.buttons}>
+					<ActionButton className='round' icon={DeleteIcon} onClick={handleDelete} />
+					<ActionButton className='round' icon={showFeedback ? arrowUp : arrowSelect} onClick={() => setShowFeedback(!showFeedback)} />
+				</div>
+			</div>
+			{showFeedback && (
+				<>
+					<p>{interviewSession.evaluationResult.globalEvaluation.feedback}</p>
+					<CtaButton label='Dettagli' className='ctaC' onClick={() => setSelectedInterviewSession(interviewSession)} />
+				</>
+			)}
+		</div>
+	);
 };
 
 export default QuizCard;
