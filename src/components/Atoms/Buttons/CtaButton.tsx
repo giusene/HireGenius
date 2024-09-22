0b5da283c913797018ef0372f6033e90ba26a1fa
majinbru <majinbrum@gmail.com>
2024-09-23 00:16:42 +0200

    disabled cta

--- src/components/Atoms/Buttons/CtaButton.tsx
+++ src/components/Atoms/Buttons/CtaButton.tsx
@@ -5,14 +5,15 @@ interface ButtonProps {
 	className: string;
 	onClick?: () => void;
 	type?: "submit" | "reset" | "button" | undefined;
+	disabled?: boolean;
 }
 
 const CtaButton = (props: ButtonProps) => {
-	const { label, className, onClick, type } = props;
+	const { label, className, onClick, type, disabled = false } = props;
 
 	return (
 		<>
-			<button className={`${style.button} ${style.ctaButton} ${className && style[className]}`} onClick={onClick} type={type}>
+			<button className={`${style.button} ${style.ctaButton} ${className && style[className]}`} onClick={onClick} type={type} disabled={disabled}>
 				{label}
 			</button>
 		</>
