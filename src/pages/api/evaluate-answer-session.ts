3335fdf9abc3444120035bee9974fe8810c6ca1c
Giovanni Raniolo <gianniraniolo80@gmail.com>
2024-09-24 10:13:16 +0200

    feat: Switch AI model from Pro to Flash and improve evaluation prompt engineering

--- src/pages/api/evaluate-answer-session.ts
+++ src/pages/api/evaluate-answer-session.ts
@@ -3,79 +3,79 @@ import { GoogleGenerativeAI, SchemaType } from "@google/generative-ai";
 import { QuizResponse } from "@/interfaces/interfaces";
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-	console.log("Handler chiamato", req.body); // Log richieste
+  console.log("Handler chiamato", req.body); // Log richieste
 
-	if (req.method === "POST") {
-		try {
-			const { prompt, quizResponses } = req.body as { prompt: string; quizResponses: QuizResponse[] };
+  if (req.method === "POST") {
+    try {
+      const { prompt, quizResponses } = req.body as { prompt: string; quizResponses: QuizResponse[] };
 
-			if (!quizResponses || !Array.isArray(quizResponses) || !prompt) {
-				return res.status(400).json("Invalid request data.");
-			}
+      if (!quizResponses || !Array.isArray(quizResponses) || !prompt) {
+        return res.status(400).json("Invalid request data.");
+      }
 
-			if (process.env.NEXT_PUBLIC_GEMINI_KEY) {
-				const genAI = new GoogleGenerativeAI(process.env.NEXT_PUBLIC_GEMINI_KEY);
+      if (process.env.NEXT_PUBLIC_GEMINI_KEY) {
+        const genAI = new GoogleGenerativeAI(process.env.NEXT_PUBLIC_GEMINI_KEY);
 
-				const model = genAI.getGenerativeModel({
-					model: "gemini-1.5-pro",
-					generationConfig: {
-						responseMimeType: "application/json",
-						responseSchema: {
-							type: SchemaType.OBJECT,
-							properties: {
-								evaluatedResponses: {
-									type: SchemaType.ARRAY,
-									items: {
-										type: SchemaType.OBJECT,
-										properties: {
-											q: { type: SchemaType.STRING },
-											a: { type: SchemaType.STRING },
-											correctAnswer: { type: SchemaType.STRING },
-											answerStatus: {
-												type: SchemaType.STRING,
-												enum: ["correct", "incorrect", "average"],
-											},
-											answerFeedback: { type: SchemaType.STRING },
-										},
-										required: ["q", "a", "correctAnswer", "answerStatus", "answerFeedback"],
-									},
-								},
-								globalEvaluation: {
-									type: SchemaType.OBJECT,
-									properties: {
-										feedback: { type: SchemaType.STRING },
-										points: { type: SchemaType.INTEGER },
-										outOf: { type: SchemaType.INTEGER },
-									},
-									required: ["feedback", "points", "outOf"],
-								},
-							},
-							required: ["evaluatedResponses", "globalEvaluation"],
-						},
-					},
-				});
+        const model = genAI.getGenerativeModel({
+          model: "gemini-1.5-flash",
+          generationConfig: {
+            responseMimeType: "application/json",
+            responseSchema: {
+              type: SchemaType.OBJECT,
+              properties: {
+                evaluatedResponses: {
+                  type: SchemaType.ARRAY,
+                  items: {
+                    type: SchemaType.OBJECT,
+                    properties: {
+                      q: { type: SchemaType.STRING },
+                      a: { type: SchemaType.STRING },
+                      correctAnswer: { type: SchemaType.STRING },
+                      answerStatus: {
+                        type: SchemaType.STRING,
+                        enum: ["correct", "incorrect", "average"],
+                      },
+                      answerFeedback: { type: SchemaType.STRING },
+                    },
+                    required: ["q", "a", "correctAnswer", "answerStatus", "answerFeedback"],
+                  },
+                },
+                globalEvaluation: {
+                  type: SchemaType.OBJECT,
+                  properties: {
+                    feedback: { type: SchemaType.STRING },
+                    points: { type: SchemaType.INTEGER },
+                    outOf: { type: SchemaType.INTEGER },
+                  },
+                  required: ["feedback", "points", "outOf"],
+                },
+              },
+              required: ["evaluatedResponses", "globalEvaluation"],
+            },
+          },
+        });
 
-				const formattedPrompt = `${prompt} ${JSON.stringify(quizResponses)}`;
-				console.log("Formatted prompt:", formattedPrompt);
+        const formattedPrompt = `${prompt} ${JSON.stringify(quizResponses)}`;
+        console.log("Formatted prompt:", formattedPrompt);
 
-				const quizResponsesEvaluation = await model.generateContent(formattedPrompt);
-				console.log("Model result:", quizResponsesEvaluation.response.text());
+        const quizResponsesEvaluation = await model.generateContent(formattedPrompt);
+        console.log("Model result:", quizResponsesEvaluation.response.text());
 
-				try {
-					const evaluation = JSON.parse(quizResponsesEvaluation.response.text());
-					res.status(200).json(evaluation);
-				} catch (e) {
-					console.error("Error parsing model response:", e);
-					res.status(500).json("Error parsing model response.");
-				}
-			} else {
-				res.status(400).json("API KEY missing.");
-			}
-		} catch (e) {
-			console.error("Error evaluating answers:", e);
-			res.status(500).json("Error evaluating answers.");
-		}
-	} else {
-		res.status(405).json("Method not allowed. Only POST requests are allowed.");
-	}
+        try {
+          const evaluation = JSON.parse(quizResponsesEvaluation.response.text());
+          res.status(200).json(evaluation);
+        } catch (e) {
+          console.error("Error parsing model response:", e);
+          res.status(500).json("Error parsing model response.");
+        }
+      } else {
+        res.status(400).json("API KEY missing.");
+      }
+    } catch (e) {
+      console.error("Error evaluating answers:", e);
+      res.status(500).json("Error evaluating answers.");
+    }
+  } else {
+    res.status(405).json("Method not allowed. Only POST requests are allowed.");
+  }
 }
--- src/pages/interview-process/index.tsx
+++ src/pages/interview-process/index.tsx
@@ -18,180 +18,180 @@ import style from "./interview-process.module.scss";
 type Step = "newInterview" | "interviewRequirements" | "chooseInterviewer" | "questionCard" | "resultsList";
 
 const InterviewProcess = () => {
-	const { user } = useAuth(); // Otteniamo l'utente autenticato dal contesto
-	const [interviewOptions, setInterviewOptions] = useState<InterviewOptions | null>(null);
-	const [interviewRequirements, setInterviewRequirements] = useState("");
-	const [interviewDetails, setInterviewDetails] = useState<InterviewDetails | null>(null);
-	const [step, setStep] = useState<Step>("newInterview");
-	const [generatedQuestions, setGeneratedQuestions] = useState<GeneratedQuestion[]>([]);
-	const [quizResponses, setQuizResponses] = useState<QuizResponse[] | null>(null);
-	const [evaluationResult, setEvaluationResult] = useState<EvaluationResult | null>(null);
-	const [error, setError] = useState<string | null>(null);
-	const [isLoading, setIsLoading] = useState(false);
+  const { user } = useAuth(); // Otteniamo l'utente autenticato dal contesto
+  const [interviewOptions, setInterviewOptions] = useState<InterviewOptions | null>(null);
+  const [interviewRequirements, setInterviewRequirements] = useState("");
+  const [interviewDetails, setInterviewDetails] = useState<InterviewDetails | null>(null);
+  const [step, setStep] = useState<Step>("newInterview");
+  const [generatedQuestions, setGeneratedQuestions] = useState<GeneratedQuestion[]>([]);
+  const [quizResponses, setQuizResponses] = useState<QuizResponse[] | null>(null);
+  const [evaluationResult, setEvaluationResult] = useState<EvaluationResult | null>(null);
+  const [error, setError] = useState<string | null>(null);
+  const [isLoading, setIsLoading] = useState(false);
 
-	const handleInterviewOptionsSubmit = (interviewOptions: InterviewOptions) => {
-		setIsLoading(true);
-		setInterviewOptions(interviewOptions);
-		setStep("interviewRequirements");
-		setIsLoading(false);
-	};
+  const handleInterviewOptionsSubmit = (interviewOptions: InterviewOptions) => {
+    setIsLoading(true);
+    setInterviewOptions(interviewOptions);
+    setStep("interviewRequirements");
+    setIsLoading(false);
+  };
 
-	const handleInterviewRequirementsSubmit = (interviewRequirements: string) => {
-		setIsLoading(true);
-		setInterviewRequirements(interviewRequirements);
-		setStep("chooseInterviewer");
-		setIsLoading(false);
-	};
+  const handleInterviewRequirementsSubmit = (interviewRequirements: string) => {
+    setIsLoading(true);
+    setInterviewRequirements(interviewRequirements);
+    setStep("chooseInterviewer");
+    setIsLoading(false);
+  };
 
-	const handleInterviewerSelect = async (selectedInterviewer: Interviewer) => {
-		setIsLoading(true);
+  const handleInterviewerSelect = async (selectedInterviewer: Interviewer) => {
+    setIsLoading(true);
 
-		if (!interviewOptions) {
-			return;
-		}
+    if (!interviewOptions) {
+      return;
+    }
 
-		const newInterviewDetails = {
-			...interviewOptions,
-			interviewRequirements,
-			interviewer: selectedInterviewer,
-		};
-		setInterviewDetails(newInterviewDetails); // Imposta lo stato dell'intervista
+    const newInterviewDetails = {
+      ...interviewOptions,
+      interviewRequirements,
+      interviewer: selectedInterviewer,
+    };
+    setInterviewDetails(newInterviewDetails); // Imposta lo stato dell'intervista
 
-		const prompt = `
+    const prompt = `
 		  Immagina di essere l'esaminatore ${newInterviewDetails.interviewer.name}. ${newInterviewDetails.interviewer.longBio}.
 		  Devi condurre un colloquio tecnico ${newInterviewDetails.level}. I requisiti del colloquio sono i seguenti: ${newInterviewDetails.interviewRequirements}.
 		  Ponimi ${newInterviewDetails.numQuestions} domande tecniche sul tema "${newInterviewDetails.topic}", di difficoltà crescente.
 		`;
 
-		// Debug del prompt
-		console.log("Prompt sent to API:", prompt);
-		try {
-			const response = await fetch("/api/generate-question", {
-				method: "POST",
-				headers: {
-					"Content-Type": "application/json",
-				},
-				body: JSON.stringify({ prompt }),
-			});
+    // Debug del prompt
+    console.log("Prompt sent to API:", prompt);
+    try {
+      const response = await fetch("/api/generate-question", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ prompt }),
+      });
 
-			if (!response.ok) {
-				throw new Error("Errore nella richiesta al server.");
-			}
+      if (!response.ok) {
+        throw new Error("Errore nella richiesta al server.");
+      }
 
-			const result = await response.json();
-			const parsedData = JSON.parse(result);
-			setGeneratedQuestions(parsedData);
+      const result = await response.json();
+      const parsedData = JSON.parse(result);
+      setGeneratedQuestions(parsedData);
 
-			setStep("questionCard");
-			setIsLoading(false);
+      setStep("questionCard");
+      setIsLoading(false);
 
-			console.log("Questions generated successfully", parsedData);
-		} catch (e) {
-			setIsLoading(false);
+      console.log("Questions generated successfully", parsedData);
+    } catch (e) {
+      setIsLoading(false);
 
-			console.error("Error generating questions", e);
-		}
-	};
+      console.error("Error generating questions", e);
+    }
+  };
 
-	const evaluateAnswers = async () => {
-		setIsLoading(true);
-		setError(null);
+  const evaluateAnswers = async () => {
+    setIsLoading(true);
+    setError(null);
 
-		if (interviewDetails && quizResponses) {
-			const prompt = `Immagina di essere l'esaminatore ${interviewDetails.interviewer.name}. ${interviewDetails.interviewer.longBio}.
+    if (interviewDetails && quizResponses) {
+      const prompt = `Immagina di essere l'esaminatore ${interviewDetails.interviewer.name}. ${interviewDetails.interviewer.longBio}.
 			Valuta le seguenti risposte fornite durante un colloquio tecnico per una posizione di ${interviewDetails.topic} di livello ${interviewDetails.level}. Per ogni risposta, fornisci:
 			1. Uno status: correct | average | incorrect.
 			2. Una valutazione con breve spiegazione.
-			3. La risposta corretta.
+			3. La risposta corretta estremamente sintetica.
 			Alla fine, fornisci una sintetica valutazione globale con un punteggio finale su 100 e una breve frase che riassuma le prestazioni generali del candidato (ad esempio: "Hai superato il test", "Hai dimostrato buone competenze", "Devi migliorare").`;
 
-			try {
-				const payload = { prompt, quizResponses };
-				console.log("Payload inviato:", payload);
+      try {
+        const payload = { prompt, quizResponses };
+        console.log("Payload inviato:", payload);
 
-				const quizResponsesEvaluation = await fetch("/api/evaluate-answer-session", {
-					method: "POST",
-					headers: {
-						"Content-Type": "application/json",
-					},
-					body: JSON.stringify(payload),
-				});
+        const quizResponsesEvaluation = await fetch("/api/evaluate-answer-session", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(payload),
+        });
 
-				if (quizResponsesEvaluation.status === 429) {
-					throw new Error("Too Many Requests - Rate limit exceeded");
-				}
+        if (quizResponsesEvaluation.status === 429) {
+          throw new Error("Too Many Requests - Rate limit exceeded");
+        }
 
-				if (quizResponsesEvaluation.status >= 500) {
-					setError("Error");
-					throw new Error("Internal Server Error - Server error");
-				}
+        if (quizResponsesEvaluation.status >= 500) {
+          setError("Error");
+          throw new Error("Internal Server Error - Server error");
+        }
 
-				if (!quizResponsesEvaluation.ok) {
-					throw new Error("Errore nella richiesta di valutazione.");
-				}
+        if (!quizResponsesEvaluation.ok) {
+          throw new Error("Errore nella richiesta di valutazione.");
+        }
 
-				const evaluationResult: EvaluationResult = await quizResponsesEvaluation.json();
-				console.log("Risultato della valutazione:", evaluationResult);
-				setEvaluationResult(evaluationResult);
-				if (user) {
-					await saveInterviewSession(user.uid, interviewDetails, evaluationResult);
-					console.log("Sessione salvata con successo!");
-				}
-				setError(null);
-				setIsLoading(false);
-			} catch (e) {
-				setError("errore");
-				setIsLoading(false);
-				console.error("Errore durante la valutazione:", e);
-			} finally {
-				console.log("Fine evaluateAnswers");
-				// ================================================================
-				// Scommenta per testare l'errore
-				// setError("Error");
-				// ================================================================
-			}
-		}
-	};
+        const evaluationResult: EvaluationResult = await quizResponsesEvaluation.json();
+        console.log("Risultato della valutazione:", evaluationResult);
+        setEvaluationResult(evaluationResult);
+        if (user) {
+          await saveInterviewSession(user.uid, interviewDetails, evaluationResult);
+          console.log("Sessione salvata con successo!");
+        }
+        setError(null);
+        setIsLoading(false);
+      } catch (e) {
+        setError("errore");
+        setIsLoading(false);
+        console.error("Errore durante la valutazione:", e);
+      } finally {
+        console.log("Fine evaluateAnswers");
+        // ================================================================
+        // Scommenta per testare l'errore
+        // setError("Error");
+        // ================================================================
+      }
+    }
+  };
 
-	const handleQuestionsComplete = (quizResponsesSent: QuizResponse[]) => {
-		setStep("resultsList");
-		setIsLoading(true);
-		setError(null);
-		if (quizResponsesSent) {
-			setQuizResponses(quizResponsesSent);
-		}
-	};
+  const handleQuestionsComplete = (quizResponsesSent: QuizResponse[]) => {
+    setStep("resultsList");
+    setIsLoading(true);
+    setError(null);
+    if (quizResponsesSent) {
+      setQuizResponses(quizResponsesSent);
+    }
+  };
 
-	useEffect(() => {
-		if (quizResponses) {
-			evaluateAnswers();
-		}
-	}, [quizResponses]);
-	{
-		if (isLoading) return <Loading lazyLoading={true} />;
-	}
+  useEffect(() => {
+    if (quizResponses) {
+      evaluateAnswers();
+    }
+  }, [quizResponses]);
+  {
+    if (isLoading) return <Loading lazyLoading={true} />;
+  }
 
-	return (
-		<>
-			{step === "newInterview" && <NewInterview onSubmit={handleInterviewOptionsSubmit} />}
-			{step === "interviewRequirements" && <InterviewRequirements onSubmit={handleInterviewRequirementsSubmit} />}
-			{step === "chooseInterviewer" && interviewOptions && <ChooseInterviewer onInterviewerSelect={handleInterviewerSelect} />}
-			{step === "questionCard" && interviewDetails && (
-				<QuestionCard role={interviewDetails.topic} totalQuestions={generatedQuestions.length} questions={generatedQuestions} onComplete={handleQuestionsComplete} />
-			)}
-			{error && interviewDetails ? <ResultsListError evaluateAnswers={evaluateAnswers} interviewDetails={interviewDetails} /> : null}
-			{step === "resultsList" && evaluationResult && interviewDetails && (
-				<main className={style.main}>
-					<div className={style.container}>
-						<ResultsList evaluationResult={evaluationResult} interviewDetails={interviewDetails} />
-					</div>
-					<Link href={"/landing-page"} className={style.linkBtn}>
-						<CtaButton label='Torna alla home' className='ctaC' />
-					</Link>
-				</main>
-			)}
-		</>
-	);
+  return (
+    <>
+      {step === "newInterview" && <NewInterview onSubmit={handleInterviewOptionsSubmit} />}
+      {step === "interviewRequirements" && <InterviewRequirements onSubmit={handleInterviewRequirementsSubmit} />}
+      {step === "chooseInterviewer" && interviewOptions && <ChooseInterviewer onInterviewerSelect={handleInterviewerSelect} />}
+      {step === "questionCard" && interviewDetails && (
+        <QuestionCard role={interviewDetails.topic} totalQuestions={generatedQuestions.length} questions={generatedQuestions} onComplete={handleQuestionsComplete} />
+      )}
+      {error && interviewDetails ? <ResultsListError evaluateAnswers={evaluateAnswers} interviewDetails={interviewDetails} /> : null}
+      {step === "resultsList" && evaluationResult && interviewDetails && (
+        <main className={style.main}>
+          <div className={style.container}>
+            <ResultsList evaluationResult={evaluationResult} interviewDetails={interviewDetails} />
+          </div>
+          <Link href={"/landing-page"} className={style.linkBtn}>
+            <CtaButton label="Torna alla home" className="ctaC" />
+          </Link>
+        </main>
+      )}
+    </>
+  );
 };
 
 export default withAuth(InterviewProcess);
